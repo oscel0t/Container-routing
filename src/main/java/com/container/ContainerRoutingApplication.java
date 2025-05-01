3f011afe7f316203de8c277406f0632fb8197ef5
Amrit Raj <149646457+oscel0t@users.noreply.github.com>
2025-05-01 15:47:53 +0530

    Create ContainerRoutingApplication.java

---
+++ src/main/java/com/container/ContainerRoutingApplication.java
@@ -0,0 +1,11 @@
+package com.container;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerRoutingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContainerRoutingApplication.class, args);
+    }
+}
